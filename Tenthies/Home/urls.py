5e861e1b5fdcac1b480ac5abc18ad441faf14a3b
UG-SEP <ujjwalcomputerpro1@gmail.com>
2021-12-25 15:57:31 +0530

    Responsive SignIn SignUp etc

--- Tenthies/Home/urls.py
+++ Tenthies/Home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from Home.views import Home
+from Home.views import Construction, Home,ContactUs
 
 urlpatterns = [
-    path('',Home),
+    path('',Home,name='home'),
+    path('contact-us',ContactUs,name='contact-us'),
+    path('under-contruction',Construction,name='construction')
 ]
--- Tenthies/Home/views.py
+++ Tenthies/Home/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from Home.forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 def Home(request):
     res = render(request,'Home/index.html')
     return res
+
+def ContactUs(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            to_email = form.cleaned_data['to_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message,'ujjwalcomputerpro1@gmail.com',[to_email])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('home')
+    return render(request, 'Home/contact-us.html', {'form': form})
+
+def Construction(request):
+    return render(request,'Home/under-construction.html')
